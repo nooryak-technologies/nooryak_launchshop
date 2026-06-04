e6fff948d6b5e6c704bdf3be0cbdec86f63e7a08
samirahamath <samir52790@gmail.com>
2026-06-04 16:21:41 +0530

    add to cart pop up

---
+++ database/migrations/2026_06_04_104909_update_template_footers_contact_info.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $userIds = DB::table('users')
+            ->where('preview_template', 1)
+            ->orWhereIn('username', [
+                'grocery', 'furial', 'fashclo', 'electi', 'kidsfa', 
+                'manti', 'petrashop', 'skinflow', 'jewellery', 'clothing'
+            ])
+            ->pluck('id')
+            ->toArray();
+
+        DB::table('user_contacts')->whereIn('user_id', $userIds)->update([
+            'contact_numbers' => '6374913298',
+            'contact_mails' => 'nooryaktechnologies@gmail.com',
+            'contact_addresses' => 'Triplicane, Chennai, IND'
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Reverting this data migration is not required/supported as the template values were different.
+    }
+};
