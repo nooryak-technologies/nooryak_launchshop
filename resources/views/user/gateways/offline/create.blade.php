da4b837d5427e5e78f93347db5d09b13caeb627d
abdulbahad123 <abdulbahad.dev@gmail.com>
2026-07-13 18:05:13 +0530

    fix editor focus lock inside offline gateway create and edit modals

--- resources/views/user/gateways/offline/create.blade.php
+++ resources/views/user/gateways/offline/create.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+<div class="modal fade" id="createModal" role="dialog" aria-labelledby="exampleModalCenterTitle"
   aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
     <div class="modal-content">
--- resources/views/user/gateways/offline/edit.blade.php
+++ resources/views/user/gateways/offline/edit.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+<div class="modal fade" id="editModal" role="dialog" aria-labelledby="exampleModalCenterTitle"
   aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
     <div class="modal-content">
