1f188286767a450c93e1b7cc4ed2e31b040b7f09
abdulbahad123 <abdulbahad.dev@gmail.com>
2026-07-23 15:45:48 +0530

    Fix timezone discrepancy on order updates

--- app/Models/User/UserOrder.php
+++ app/Models/User/UserOrder.php
@@ -25,4 +25,20 @@ class UserOrder extends Model
     {
         return $this->belongsTo(Customer::class, 'customer_id', 'id');
     }
+
+    protected static function booted()
+    {
+        static::saving(function ($order) {
+            $userId = $order->user_id;
+            if ($userId) {
+                $timeZone = \DB::table('user_basic_settings')->where('user_id', $userId)->value('timezone');
+                if (!empty($timeZone)) {
+                    $order->updated_at = \Carbon\Carbon::now($timeZone);
+                    if (empty($order->id) && empty($order->created_at)) {
+                        $order->created_at = \Carbon\Carbon::now($timeZone);
+                    }
+                }
+            }
+        });
+    }
 }
