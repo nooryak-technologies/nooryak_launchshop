7bf52dfbe2a1d9138342ac617660ef2dc26741b3
abdulbahad123 <abdulbahad.dev@gmail.com>
2026-07-24 11:38:18 +0530

    File uploaded

--- resources/views/user-front/styles.blade.php
+++ resources/views/user-front/styles.blade.php
@@ -73,7 +73,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400&family=Jost:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('assets/user-front/css/clothing/clothing.css?v=1.0.4') }}">
-  <link rel="stylesheet" href="{{ asset('assets/user-front/css/clothing/custom-styles.css?v=1.0.5') }}">
+  <link rel="stylesheet" href="{{ asset('assets/user-front/css/clothing/custom-styles.css?v=1.0.6') }}">
 @endif
 <!--====== Style css ======-->
 
