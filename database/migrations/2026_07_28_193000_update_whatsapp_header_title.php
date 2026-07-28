741ec7e866336df046ac41d338d9b7ecc88e55b6
abdulbahad123 <abdulbahad.dev@gmail.com>
2026-07-28 19:16:44 +0530

    File uploaded

---
+++ database/migrations/2026_07_28_193000_update_whatsapp_header_title.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Update whatsapp_header_title in the basic_settings table for all rows
+        DB::table('basic_settings')
+            ->where('whatsapp_header_title', 'Hi, There!')
+            ->orWhere('whatsapp_header_title', 'Hi, there!')
+            ->orWhereNull('whatsapp_header_title')
+            ->update(['whatsapp_header_title' => 'LaunchShop Support']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('basic_settings')
+            ->where('whatsapp_header_title', 'LaunchShop Support')
+            ->update(['whatsapp_header_title' => 'Hi, There!']);
+    }
+};
