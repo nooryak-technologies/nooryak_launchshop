2e2861d30458995931ee79995e937d0a5a434c5f
abdulbahad123 <abdulbahad.dev@gmail.com>
2026-08-18 12:45:36 +0530

    File uploade

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -544,6 +544,7 @@ class ItemController extends Controller
             ->pluck('unique_id')->first();
 
         foreach ($languages as $language) {
+            $code = $language->code;
             $categoryId = UserItemCategory::where([['language_id', $language->id], ['unique_id', $catUnique_id]])->pluck('id')->first();
             $subcategoryId = UserItemSubCategory::where([['unique_id', $subcatUnique_id], ['language_id', $language->id]])->pluck('id')->first();
 
@@ -558,6 +559,7 @@ class ItemController extends Controller
                 $adContent->language_id = $language->id;
             }
             if (
+                $language->is_default == 1 ||
                 $request->input($code . '_title') ||
                 $request->input($code . '_label_id') ||
                 $request->input($code . '_summary') ||
@@ -567,13 +569,13 @@ class ItemController extends Controller
             ) {
                 $adContent->category_id = $categoryId;
                 $adContent->subcategory_id = $subcategoryId;
-                $adContent->label_id = $request[$language->code . '_label_id'];
-                $adContent->title = $request[$language->code . '_title'];
-                $adContent->slug = make_slug($request[$language->code . '_title']);
-                $adContent->summary = Purifier::clean($request[$language->code . '_summary'], 'youtube');
-                $adContent->description = Purifier::clean($request[$language->code . '_description'], 'youtube');
-                $adContent->meta_keywords = $request[$language->code . '_meta_keywords'];
-                $adContent->meta_description = $request[$language->code . '_meta_description'];
+                $adContent->label_id = $request[$code . '_label_id'];
+                $adContent->title = $request[$code . '_title'];
+                $adContent->slug = make_slug($request[$code . '_title']);
+                $adContent->summary = Purifier::clean($request[$code . '_summary'], 'youtube');
+                $adContent->description = Purifier::clean($request[$code . '_description'], 'youtube');
+                $adContent->meta_keywords = $request[$code . '_meta_keywords'];
+                $adContent->meta_description = $request[$code . '_meta_description'];
                 $adContent->save();
             }
         }
